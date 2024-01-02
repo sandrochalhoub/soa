6c04eb534571381ce19ae0e46cf943c8242fab5a
sarah <mousset@insa-toulouse.fr>
2024-01-02 18:29:21 +0100

    adding mission class

---
+++ soap/src/main/java/fr/insa/ws/soap/Mission.java
@@ -0,0 +1,31 @@
+package fr.insa.ws.soap;
+
+import java.util.UUID;
+
+public class Mission {
+    private String description;
+    private String id;
+    private int status; // 0: en attente, 1: validée, 2: réalisée
+
+    public Mission(String description) {
+        this.description = description;
+        this.id = UUID.randomUUID().toString();
+        this.status = 0; // Par défaut, la mission est en attente lors de la création
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
--- soap/src/main/java/fr/insa/ws/soap/UserWS.java
+++ soap/src/main/java/fr/insa/ws/soap/UserWS.java
@@ -1,9 +1,13 @@
 package fr.insa.ws.soap;
 
 import javax.jws.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@WebService(serviceName="user")
+@WebService(serviceName = "user")
 public class UserWS {
+    private List<User> users = new ArrayList<User>();
+    private List<Mission> missions = new ArrayList<Mission>();
 	
 	@WebMethod(operationName="manageHelpRequester")
 	public String manageHelpRequester(String name) {
@@ -25,4 +29,30 @@ public class UserWS {
 		System.out.println("Added admin with ID: " + user.getID());
 		return user.getID();
 	}
+	
+	@WebMethod(operationName = "requestHelp")
+    public String requestHelp(String userId, String description) {
+        // Check the user exists and is a HelpRequester
+        User user = getUserById(userId);
+
+        if (user instanceof HelpRequester) {
+            Mission mission = new Mission(description);
+            missions.add(mission);
+            System.out.println("Help request added with ID: " + mission.getId());
+            return mission.getId();
+        } else {
+            System.out.println("User is not a HelpRequester. Cannot request help.");
+            return null;
+        }
+    }
+
+    @WebMethod(operationName = "getUserById")
+    public User getUserById(String userId) {
+        for (User user : users) {
+            if (user.getID().equals(userId)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
